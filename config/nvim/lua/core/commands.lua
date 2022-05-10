c3e2525dc4ae6f31f60c16923168767ccd9930f5
Chris Erin <chris.erin@gmail.com>
2022-05-10 08:09:12 -0500

    Easy access to zsh files

--- config/nvim/lua/core/commands.lua
+++ config/nvim/lua/core/commands.lua
@@ -12,9 +12,13 @@ cmd('CopyPath', "let @+=expand('%:h')", {})
 cmd('CopyFileName', "let @+=expand('%:t')", {})
 
 cmd('RefreshJsonSchemas', utils.download_json_schemas, {})
+
+-- easy access commands
 cmd('Plugins', "edit ~/.config/nvim/lua/plugins/init.lua", {})
 cmd('Mappings', "edit ~/.config/nvim/lua/core/mappings.lua", {})
 cmd('Commands', "edit ~/.config/nvim/lua/core/commands.lua", {})
 cmd('Options', "edit ~/.config/nvim/lua/core/options.lua", {})
 cmd('VimNotes', "edit ~/.config/nvim/vim-notes.txt", {})
 cmd('LuaNotes', "edit ~/.config/nvim/lua/lua-notes.lua", {})
+cmd('Zsh', "edit ~/.zshrc", {})
+cmd('ZshLocal', "edit ~/.zshrc.local", {})
