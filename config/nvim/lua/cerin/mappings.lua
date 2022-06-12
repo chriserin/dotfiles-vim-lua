e4d0192aaf3661f3d9a56c894cb017d1e619fa6c
Chris Erin <chris.erin@gmail.com>
2022-06-12 11:36:54 -0500

    Specific find_files with Telescope

--- config/nvim/lua/cerin/mappings.lua
+++ config/nvim/lua/cerin/mappings.lua
@@ -1,9 +1,24 @@
 
-local keyopts = { noremap = true, silent = true }
+local nmap = function(tbl)
+  vim.keymap.set('n', tbl[1], tbl[2], tbl[3])
+end
+
+local default_opts = { noremap = true, silent = true }
 vim.keymap.set({'n', 'v', 'o'}, 'H', require('cerin.tree-climber').goto_parent)
-vim.keymap.set({'n', 'v', 'o'}, 'L', require('cerin.tree-climber').goto_child, keyopts)
-vim.keymap.set({'n', 'v', 'o'}, 'J', require('cerin.tree-climber').goto_next, keyopts)
-vim.keymap.set({'n', 'v', 'o'}, 'K', require('cerin.tree-climber').goto_prev, keyopts)
-vim.keymap.set('n', '<c-k>', require('cerin.tree-climber').swap_prev, keyopts)
-vim.keymap.set('n', '<c-j>', require('cerin.tree-climber').swap_next, keyopts)
-vim.keymap.set('n', '<leader>j', require('cerin.tsquery_jest_runner').run_jest_test, keyopts)
+vim.keymap.set({'n', 'v', 'o'}, 'L', require('cerin.tree-climber').goto_child, default_opts)
+vim.keymap.set({'n', 'v', 'o'}, 'J', require('cerin.tree-climber').goto_next, default_opts)
+vim.keymap.set({'n', 'v', 'o'}, 'K', require('cerin.tree-climber').goto_prev, default_opts)
+vim.keymap.set('n', '<c-k>', require('cerin.tree-climber').swap_prev, default_opts)
+vim.keymap.set('n', '<c-j>', require('cerin.tree-climber').swap_next, default_opts)
+vim.keymap.set('n', '<leader>j', require('cerin.tsquery_jest_runner').run_jest_test, default_opts)
+
+local M = {}
+
+M.telescope_mappings = function()
+  nmap { '<leader>fc', require('cerin.telescope').find_controllers, default_opts }
+  nmap { '<leader>fv', require('cerin.telescope').find_views, default_opts }
+  nmap { '<leader>fj', require('cerin.telescope').find_js_files, default_opts }
+  nmap { '<leader>ft', require('cerin.telescope').find_js_test_files, default_opts }
+end
+
+return M
