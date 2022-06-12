d8c283e284a7fe0129d3c723853e5673d7c9f319
Chris Erin <chris.erin@gmail.com>
2022-06-12 09:16:42 -0500

    Experiment with tree climber

--- config/nvim/lua/cerin/mappings.lua
+++ config/nvim/lua/cerin/mappings.lua
@@ -1,6 +1,9 @@
 
--- M.jester_mappings = function()
---   nmap { '<leader>j', ":lua require('jester').run()<cr>" }
--- end
-
-vim.keymap.set('n', '<leader>j', ":lua require('cerin.tsquery_jest_runner').run_jest_test()<cr>")
+local keyopts = { noremap = true, silent = true }
+vim.keymap.set({'n', 'v', 'o'}, 'H', require('cerin.tree-climber').goto_parent)
+vim.keymap.set({'n', 'v', 'o'}, 'L', require('cerin.tree-climber').goto_child, keyopts)
+vim.keymap.set({'n', 'v', 'o'}, 'J', require('cerin.tree-climber').goto_next, keyopts)
+vim.keymap.set({'n', 'v', 'o'}, 'K', require('cerin.tree-climber').goto_prev, keyopts)
+vim.keymap.set('n', '<c-k>', require('cerin.tree-climber').swap_prev, keyopts)
+vim.keymap.set('n', '<c-j>', require('cerin.tree-climber').swap_next, keyopts)
+vim.keymap.set('n', '<leader>j', require('cerin.tsquery_jest_runner').run_jest_test, keyopts)
