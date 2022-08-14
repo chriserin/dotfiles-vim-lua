e89b1f12384e24b55701846c8ba6f9ae8034162b
Chris Erin <chris.erin@gmail.com>
2022-08-14 11:18:36 -0500

    Dis+En blank line commands

--- config/nvim/lua/cerin/commands.lua
+++ config/nvim/lua/cerin/commands.lua
@@ -1,23 +1,26 @@
 local cmd = vim.api.nvim_create_user_command
 
-cmd('SqlFormat', "!sqlformat -a %", {})
+cmd('SqlFormat', '!sqlformat -a %', {})
 
 -- easy access commands
-cmd('Plugins', "edit ~/.config/nvim/lua/plugins/init.lua", {})
-cmd('Mappings', "edit ~/.config/nvim/lua/core/mappings.lua", {})
-cmd('Commands', "edit ~/.config/nvim/lua/core/commands.lua", {})
-cmd('Options', "edit ~/.config/nvim/lua/core/options.lua", {})
-cmd('VimNotes', "edit ~/.config/nvim/vim-notes.txt", {})
-cmd('LuaNotes', "edit ~/.config/nvim/lua/lua-notes.lua", {})
-cmd('Zsh', "edit ~/.zshrc", {})
-cmd('ZshLocal', "edit ~/.zshrc.local", {})
+cmd('Plugins', 'edit ~/.config/nvim/lua/plugins/init.lua', {})
+cmd('Mappings', 'edit ~/.config/nvim/lua/core/mappings.lua', {})
+cmd('Commands', 'edit ~/.config/nvim/lua/core/commands.lua', {})
+cmd('Options', 'edit ~/.config/nvim/lua/core/options.lua', {})
+cmd('VimNotes', 'edit ~/.config/nvim/vim-notes.txt', {})
+cmd('LuaNotes', 'edit ~/.config/nvim/lua/lua-notes.lua', {})
+cmd('Zsh', 'edit ~/.zshrc', {})
+cmd('ZshLocal', 'edit ~/.zshrc.local', {})
 
 cmd(
-  "PackerEdit",
+  'PackerEdit',
   "lua require('cerin.utils').edit_plugin(<f-args>)",
-  {complete = require("packer").plugin_complete, nargs = 1}
+  { complete = require('packer').plugin_complete, nargs = 1 }
 )
 
-cmd("Glow", "lua require('cerin.utils').glow(<f-args>)", {complete = 'file', nargs = '?'})
+cmd('Glow', "lua require('cerin.utils').glow(<f-args>)", { complete = 'file', nargs = '?' })
 
 cmd('W', ':noa write', {})
+
+cmd('DisableIndentLines', "lua require('indent_blankline.commands').disable()", {})
+cmd('EnableIndentLines', "lua require('indent_blankline.commands').enable()", {})
