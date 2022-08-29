2db387f78f6110b3b1cf248a89c921899dfedbcd
Chris Erin <chris.erin@gmail.com>
2022-08-29 07:13:56 -0500

    Close all float windows with LDR-cc

--- config/nvim/lua/cerin/mappings.lua
+++ config/nvim/lua/cerin/mappings.lua
@@ -1,15 +1,16 @@
-
 local nmap = function(tbl)
   vim.keymap.set('n', tbl[1], tbl[2], tbl[3])
 end
 
 local default_opts = { noremap = true, silent = true }
-vim.keymap.set({'n', 'v', 'o'}, 'H', require('cerin.tree-climber').goto_parent)
-vim.keymap.set({'n', 'v', 'o'}, 'L', require('cerin.tree-climber').goto_child, default_opts)
-vim.keymap.set({'n', 'v', 'o'}, 'K', require('cerin.tree-climber').goto_prev, default_opts)
+
+vim.keymap.set({ 'n', 'v', 'o' }, 'H', require('cerin.tree-climber').goto_parent)
+vim.keymap.set({ 'n', 'v', 'o' }, 'L', require('cerin.tree-climber').goto_child, default_opts)
+vim.keymap.set({ 'n', 'v', 'o' }, 'K', require('cerin.tree-climber').goto_prev, default_opts)
 vim.keymap.set('n', '<c-k>', require('cerin.tree-climber').swap_prev, default_opts)
 vim.keymap.set('n', '<c-j>', require('cerin.tree-climber').swap_next, default_opts)
 vim.keymap.set('n', '<leader>j', require('cerin.tsquery_jest_runner').run_jest_test, default_opts)
+nmap { '<leader>cc', require('cerin.utils').close_all_float_windows, default_opts }
 
 local M = {}
 
