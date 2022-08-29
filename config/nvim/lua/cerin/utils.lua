779b20f2c751470eb8c514fa243111ffdd2eec01
Chris Erin <chris.erin@gmail.com>
2022-08-29 07:13:56 -0500

    Close all float windows with LDR-cc

--- config/nvim/lua/cerin/utils.lua
+++ config/nvim/lua/cerin/utils.lua
@@ -1,19 +1,19 @@
 local M = {}
 
 function M.edit_plugin(plugin_name)
-  local packer = require('packer')
+  local packer = require 'packer'
   local packer_root = packer.config.package_root
 
-  vim.cmd('edit ' .. packer_root .. '/packer/start/' ..  plugin_name)
+  vim.cmd('edit ' .. packer_root .. '/packer/start/' .. plugin_name)
 end
 
 local open_glow = function(path)
-  vim.cmd("edit term://glow " .. path)
+  vim.cmd('edit term://glow ' .. path)
 end
 
 local normalize_path = function(path)
-  if(path == nil) then
-    return vim.fn.expand('%')
+  if path == nil then
+    return vim.fn.expand '%'
   else
     return path
   end
@@ -22,6 +22,16 @@ end
 function M.glow(path)
   local normalizedPath = normalize_path(path)
   open_glow(normalizedPath)
+end
+
+function M.close_all_float_windows()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local config = vim.api.nvim_win_get_config(win)
+    if config.relative ~= '' then
+      vim.api.nvim_win_close(win, false)
+      print('Closing window', win)
+    end
+  end
 end
 
 return M
