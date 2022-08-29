a097b83dfeffb6b6e81fc7ce2019499e2ea472a8
Chris Erin <chris.erin@gmail.com>
2022-08-29 07:13:56 -0500

    Close all float windows with LDR-cc

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -293,7 +293,7 @@ M.telescope_mappings = {
   -- ds = document symbols
   { '<leader>ds', telescope 'lsp_document_symbols', desc = '[D]ocument [S]ymbols' },
 
-  { '<leader>cc', '<cmd>Telescope conventional_commits<cr>', desc = '[C]onventional [C]ommits' },
+  -- { '<leader>cc', '<cmd>Telescope conventional_commits<cr>', desc = '[C]onventional [C]ommits' },
 
   -- search unicode symbols 
   { '<leader>fu', '<cmd>Telescope symbols<cr>', desc = '[F]ind [U]nicode' },
