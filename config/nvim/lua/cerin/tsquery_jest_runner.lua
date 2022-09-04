89c018ad832bd78bc3d95cbb2b0c95b08a473f71
Chris Erin <chris.erin@gmail.com>
2022-09-04 10:13:37 -0500

    Add typescript to allowed runner langs

--- config/nvim/lua/cerin/tsquery_jest_runner.lua
+++ config/nvim/lua/cerin/tsquery_jest_runner.lua
@@ -1,4 +1,3 @@
-
 local M = {}
 
 local describe_it_query = [[
@@ -27,14 +26,14 @@ local describe_it_query = [[
 --
 local function get_parser_language()
   local bnr = vim.api.nvim_get_current_buf()
-  local ft = vim.api.nvim_buf_get_option(bnr, "filetype")
-  return require("nvim-treesitter.parsers").ft_to_lang(ft)
+  local ft = vim.api.nvim_buf_get_option(bnr, 'filetype')
+  return require('nvim-treesitter.parsers').ft_to_lang(ft)
 end
 
 function M.get_tests()
   local bnr = vim.api.nvim_get_current_buf()
   local parser_language = get_parser_language()
-  local q = require("vim.treesitter.query")
+  local q = require 'vim.treesitter.query'
   local language_tree = vim.treesitter.get_parser(bnr, parser_language)
   local syntax_tree = language_tree:parse()
 
@@ -47,15 +46,18 @@ function M.get_tests()
     local test_table = {
       describe_start = captures[6]:start(),
       describe_end = captures[6]:end_(),
-      describes = {q.get_node_text(captures[2], bnr)},
+      describes = { q.get_node_text(captures[2], bnr) },
       it = q.get_node_text(captures[4], bnr),
       it_start = captures[5]:start(),
-      it_end = captures[5]:end_()
+      it_end = captures[5]:end_(),
     }
 
     local last_desc
     for _, existing_block in ipairs(result) do
-      if(existing_block.describe_start < test_table.describe_start and existing_block.describe_end > test_table.describe_end) then
+      if
+        existing_block.describe_start < test_table.describe_start
+        and existing_block.describe_end > test_table.describe_end
+      then
         local parent_describes = existing_block.describes
         for _, desc in ipairs(parent_describes) do
           if last_desc ~= desc then
@@ -67,25 +69,25 @@ function M.get_tests()
     end
 
     local descs = test_table.describes
-    local desc_string = ""
-    for j=#descs,1, -1 do
+    local desc_string = ''
+    for j = #descs, 1, -1 do
       local part = test_table.describes[j]
       desc_string = desc_string .. ' ' .. string.sub(part, 2, #part - 1)
     end
     local it_string = test_table.it
     local test_string = desc_string .. ' ' .. string.sub(it_string, 2, #it_string - 1)
-    test_table["test_string"] = string.sub(test_string, 2, #test_string)
+    test_table['test_string'] = string.sub(test_string, 2, #test_string)
 
     table.insert(result, test_table)
   end
 
-  return result;
+  return result
 end
 
 function M.find_test_string()
   local tests = M.get_tests()
-  if (#tests == 0) then
-    return "no-tests"
+  if #tests == 0 then
+    return 'no-tests'
   end
 
   local line_num = vim.api.nvim_win_get_cursor(0)[1]
@@ -96,31 +98,31 @@ function M.find_test_string()
     end
   end
 
-  return ""
+  return ''
 end
 
 function M.get_output_string()
   local test_string = M.find_test_string()
-  if (test_string == "no-tests") then
-    return "no-tests"
+  if test_string == 'no-tests' then
+    return 'no-tests'
   end
 
-  local file = vim.fn.expand('%:p')
+  local file = vim.fn.expand '%:p'
 
-  return "yarn --cwd=assets test " .. file .. " -t \"" .. test_string .. "\""
+  return 'yarn --cwd=assets test ' .. file .. ' -t "' .. test_string .. '"'
 end
 
 function M.run_jest_test()
   local parser_language = get_parser_language()
 
-  if not (parser_language == "tsx" or parser_language == "javascript") then
-    print("Not a jest language")
+  if not (parser_language == 'typescript' or parser_language == 'tsx' or parser_language == 'javascript') then
+    print('Not a jest language: ' .. parser_language)
     return
   end
 
   local yarn_string = M.get_output_string()
-  if (yarn_string == "no-tests") then
-    print("No jest tests")
+  if yarn_string == 'no-tests' then
+    print 'No jest tests'
   else
     vim.api.nvim_command(':Tmux ' .. yarn_string)
   end
