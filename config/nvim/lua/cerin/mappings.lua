9b4923fabf1b48db2a7e6c262bdcc4816d71fe99
Chris Erin <chris.erin@gmail.com>
2022-10-29 08:18:51 -0500

    Remove tree-climber

--- config/nvim/lua/cerin/mappings.lua
+++ config/nvim/lua/cerin/mappings.lua
@@ -4,11 +4,6 @@ end
 
 local default_opts = { noremap = true, silent = true }
 
-vim.keymap.set({ 'n', 'v', 'o' }, 'H', require('cerin.tree-climber').goto_parent)
-vim.keymap.set({ 'n', 'v', 'o' }, 'L', require('cerin.tree-climber').goto_child, default_opts)
-vim.keymap.set({ 'n', 'v', 'o' }, 'K', require('cerin.tree-climber').goto_prev, default_opts)
-vim.keymap.set('n', '<c-k>', require('cerin.tree-climber').swap_prev, default_opts)
-vim.keymap.set('n', '<c-j>', require('cerin.tree-climber').swap_next, default_opts)
 vim.keymap.set('n', '<leader>j', require('cerin.tsquery_jest_runner').run_jest_test, default_opts)
 
 nmap { '<leader>cc', require('cerin.utils').close_all_float_windows, default_opts }
