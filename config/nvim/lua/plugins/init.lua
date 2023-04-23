4789dd49495ab76d1a0354f6e40b349a94e1da97
Chris Erin <chris.erin@gmail.com>
2023-04-23 10:09:31 -0500

    Add NeoAI to nvim

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -833,4 +833,32 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'Bryley/neoai.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+    cmd = {
+      'NeoAI',
+      'NeoAIOpen',
+      'NeoAIClose',
+      'NeoAIToggle',
+      'NeoAIContext',
+      'NeoAIContextOpen',
+      'NeoAIContextClose',
+      'NeoAIInject',
+      'NeoAIInjectCode',
+      'NeoAIInjectContext',
+      'NeoAIInjectContextCode',
+    },
+    keys = {
+      { '<leader>as', desc = 'summarize text' },
+      { '<leader>ag', desc = 'generate git message' },
+    },
+    config = function()
+      require('neoai').setup {
+        -- Options go here
+      }
+    end,
+  },
 })
