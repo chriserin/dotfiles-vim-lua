b40595ed361e39ac52a15aa704094faaf38fe888
Dorian Karter <dkarter@users.noreply.github.com>
2023-09-04 15:14:14 +0000

    Check for op command

--- config/zsh/op.zsh
+++ config/zsh/op.zsh
@@ -1,8 +1,11 @@
-eval "$(op completion zsh)"
-compdef _op op
+if type "op" > /dev/null; then
+  eval "$(op completion zsh)"
+  compdef _op op
 
-# Enable SSH Agent for all hosts
-export SSH_AUTH_SOCK=~/.1password/agent.sock
 
-# prevent an issue with TELEPORT conflicting with 1password agent
-export TELEPORT_ADD_KEYS_TO_AGENT=no
+  # Enable SSH Agent for all hosts
+  export SSH_AUTH_SOCK=~/.1password/agent.sock
+
+  # prevent an issue with TELEPORT conflicting with 1password agent
+  export TELEPORT_ADD_KEYS_TO_AGENT=no
+fi
