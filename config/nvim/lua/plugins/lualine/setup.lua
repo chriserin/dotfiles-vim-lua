4f25abe0405a3df1f115ce238c26678a189d5019
Chris Erin <chris.erin@gmail.com>
2023-09-09 13:40:38 +0000

    Disable lualine winbar

--- config/nvim/lua/plugins/lualine/setup.lua
+++ config/nvim/lua/plugins/lualine/setup.lua
@@ -141,6 +141,7 @@ M.setup = function()
 
   ---@module "lualine"
   lualine.setup {
+<<<<<<< HEAD
     winbar = winbar,
     inactive_winbar = {
       lualine_a = {
@@ -159,6 +160,31 @@ M.setup = function()
           color = { fg = colors.grey },
         },
       },
+    winbar = {
+      -- lualine_a = {
+      --   {
+      --     'filetype',
+      --     colored = true, -- Displays filetype icon in color if set to true
+      --     icon_only = true, -- Display only an icon for filetype
+      --     icon = { align = 'right' }, -- Display filetype icon on the right hand side
+      --     color = { bg = colors.grey, fg = colors.black },
+      --     -- icon =    {'X', align='right'}
+      --     -- Icon string ^ in table is ignored in filetype component
+      --   },
+      --   { 'filename', separator = { right = '' }, color = { bg = colors.grey, fg = colors.black } },
+      -- },
+      -- lualine_b = {},
+      -- lualine_c = {},
+      -- lualine_x = {
+      --   -- show available updates
+      --   {
+      --     require('lazy.status').updates,
+      --     cond = require('lazy.status').has_updates,
+      --     color = { fg = '#ff9e64' },
+      --   },
+      -- },
+      -- lualine_y = {},
+      -- lualine_z = {},
     },
     options = {
       theme = 'tokyonight-moon',
