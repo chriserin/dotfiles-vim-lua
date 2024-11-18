a268d14e3f17b4e1b39b16047ab291dbe59c2a90
Chris Erin <chris.erin@gmail.com>
2024-11-18 21:51:07 +0000

    feat(ollama): Prefer qwen2.5-coder

--- config/nvim/lua/plugins/gen.lua
+++ config/nvim/lua/plugins/gen.lua
@@ -5,7 +5,7 @@ return {
   cmd = { 'Gen' },
   keys = require('core.mappings').gen_nvim_mappings,
   opts = {
-    model = 'mistral',
+    model = 'qwen2.5-coder',
     display_mode = 'split', -- The display mode. Can be "float" or "split".
     show_prompt = true, -- Shows the prompt submitted to Ollama.
     show_model = true, -- Displays which model you are using at the beginning of your chat session.
